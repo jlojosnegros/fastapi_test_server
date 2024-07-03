27ba96eb3c479ff041331a28765f57a278365b13
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2024-07-03 21:50:02 +0200

    it is possible to use a path
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- server_test/main.py
+++ server_test/main.py
@@ -36,3 +36,7 @@ async def get_model(model_name: ModelName.ModelName):
         return {"model_name": model_name, "message": "LeCNN"}
 
     return {"model_name": model_name, "message": "residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
