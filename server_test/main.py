b9c4cb7799ba57505fdfa2ca5373ab750f4fd1e4
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2024-07-03 21:15:40 +0200

    get path variable
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- server_test/main.py
+++ server_test/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def main_route():
     return {"message": "Hello there!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
